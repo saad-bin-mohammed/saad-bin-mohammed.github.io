11a014ec15cb6aacb2e09bb7a790934d6656e485
saad-bin-mohammed <94010723+saad-bin-mohammed@users.noreply.github.com>
2022-04-22 03:38:23 +0530

    completed alert message div manipultaion

--- docs/qrcode.js
+++ docs/qrcode.js
@@ -2,16 +2,23 @@
 let button = document.getElementById("button");
 let img = document.getElementById("qrimg");
 let img_container = document.getElementById("img_container");
-button.addEventListener("click", function () {
+let input_area = document.getElementById("input");
+let alert_container = document.getElementById("alert_container");
+const generatecode = function () {
   let input = document.getElementById("input");
   let download_button = document.getElementById("download_button");
   let spinnerdiv = document.getElementById("spinnerdiv");
-  if (input.value == null) {
-    return;
+  if (!input.value) {
+    console.log("in if");
+    alert_container.classList.remove("hide-alert");
+    ////////////////////////////////////////
+    ////////////////////////////////////////
+    img_container.classList.add("hide");
   } else {
     spinnerdiv.classList.remove("hide");
     setTimeout(() => {
       img_container.classList.remove("hide");
+      alert_container.classList.add("hide-alert");
       img.classList.remove("hide");
       spinnerdiv.classList.add("hide");
       img.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
@@ -19,5 +26,16 @@ button.addEventListener("click", function () {
     setTimeout(() => {
       input.value = "";
     }, 2000);
+  }
+};
+//////////////////////////////////////////////////////////////
+// EVENT LISTENERS ON BUTTON AND INPUT FIELD(WHEN ENTER IS PRESSED)
+button.addEventListener("click", generatecode); //listening to click event on [generate code  button]
+//listening to keypress event on [text input field]
+input_area.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    generatecode();
+  } else {
+    return;
   }
 });
