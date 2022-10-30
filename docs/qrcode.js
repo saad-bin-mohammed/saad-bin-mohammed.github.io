daf84153589f438fe80580cdf3e39703ff141b74
saad-bin-mohammed <94010723+saad-bin-mohammed@users.noreply.github.com>
2022-10-30 19:21:38 +0530

    code refactor

--- docs/qrcode.js
+++ docs/qrcode.js
@@ -4,6 +4,7 @@ let img = document.getElementById("qrimg");
 let img_container = document.getElementById("img_container");
 let input_area = document.getElementById("input");
 let alert_container = document.getElementById("alert_container");
+let source;
 const generatecode = function () {
   let input = document.getElementById("input");
   let download_button = document.getElementById("download_button");
@@ -18,13 +19,12 @@ const generatecode = function () {
       console.log("on loaded");
       img_container.classList.remove("hide");
       alert_container.classList.add("hide-alert");
-      console.log("on loaded second");
       img.classList.remove("hide");
       spinnerdiv.classList.add("hide");
     };
     spinnerdiv.classList.remove("hide");
-
     img.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+    source = img.src;
   }
 };
 //////////////////////////////////////////////////////////////
@@ -34,7 +34,5 @@ button.addEventListener("click", generatecode); //listening to click event on [g
 input_area.addEventListener("keypress", function (e) {
   if (e.key == "Enter") {
     generatecode();
-  } else {
-    return;
   }
 });
