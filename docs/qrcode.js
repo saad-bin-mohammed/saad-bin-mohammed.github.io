c5b3b2988bafe513e650db22145318ec79e42c1a
saad-bin-mohammed <94010723+saad-bin-mohammed@users.noreply.github.com>
2022-11-03 20:45:06 +0530

    download fuctionality

--- docs/qrcode.js
+++ docs/qrcode.js
@@ -2,37 +2,53 @@
 let button = document.getElementById("button");
 let img = document.getElementById("qrimg");
 let img_container = document.getElementById("img_container");
-let input_area = document.getElementById("input");
-let alert_container = document.getElementById("alert_container");
+let input = document.getElementById("input");
+let download_button = document.getElementById("download_button");
 let source;
 const generatecode = function () {
   let input = document.getElementById("input");
-  let download_button = document.getElementById("download_button");
   let spinnerdiv = document.getElementById("spinnerdiv");
   if (!input.value) {
-    alert_container.classList.remove("hide-alert");
-    ////////////////////////////////////////
-    ////////////////////////////////////////
     img_container.classList.add("hide");
+    download_button.classList.add("hide");
+    return;
   } else {
     img.onload = function () {
-      console.log("on loaded");
       img_container.classList.remove("hide");
-      alert_container.classList.add("hide-alert");
       img.classList.remove("hide");
       spinnerdiv.classList.add("hide");
     };
     spinnerdiv.classList.remove("hide");
     img.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
     source = img.src;
+    setTimeout(() => {
+      download_button.classList.remove("hide");
+    }, "1000");
   }
 };
+
 //////////////////////////////////////////////////////////////
 // EVENT LISTENERS ON BUTTON AND INPUT FIELD(WHEN ENTER IS PRESSED)
 button.addEventListener("click", generatecode); //listening to click event on [generate code  button]
 //listening to keypress event on [text input field]
-input_area.addEventListener("keypress", function (e) {
+input.addEventListener("keypress", function (e) {
   if (e.key == "Enter") {
     generatecode();
   }
 });
+download_button.addEventListener("click", function () {
+  fetchFile(source);
+});
+function fetchFile(url) {
+  fetch(url)
+    .then((res) => res.blob())
+    .then((file) => {
+      let locator = URL.createObjectURL(file);
+      let a = document.createElement("a");
+      a.href = locator;
+      a.download = "Qr code";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    });
+}
