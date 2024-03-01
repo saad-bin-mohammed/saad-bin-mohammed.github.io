ac615dc161f3ae2c0758729487f984e43c1eb498
saad-bin-mohammed <94010723+saad-bin-mohammed@users.noreply.github.com>
2024-03-01 21:30:46 +0530

    REFACTOR

--- docs/qrcode.js
+++ docs/qrcode.js
@@ -1,58 +1,28 @@
-let input = document.querySelector(".input-area");
-let section = document.querySelector("section");
 let spinner = document.querySelector(".spinner");
-let generate_button = document.querySelector(".generate-code-button");
+let form = document.querySelector("form");
 let blob;
-generate_button.addEventListener("click", function fetch() {
-  input.value ? fetchdata() : null;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(form);
+  fetchdata(formData.get("input"));
 });
-input?.addEventListener("keypress", (e) => {
-  if (e.key == "Enter") {
-    if (input.value) {
-      fetchdata();
-    } else {
-      document.querySelector(".main_div")?.remove();
-    }
-  }
-});
-const fetchdata = async function () {
+const fetchdata = async function (input) {
   spinner.classList.remove("hide");
   const response = await fetch(
     ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
-      input.value
+      input
     )}`
   );
   blob = response.blob();
-  let main_div = document.querySelector(".main_div");
-  if (main_div) {
-    main_div.querySelector("img").src = response.url;
-    spinner.classList.add("hide");
+  let generatedImage = document.querySelector("#generatedImage");
+  if (generatedImage) {
+    generatedImage.src = response.url;
   } else {
-    let html = `
-      <div class="main_div">
-           <div class="image-container" id="img_container">
-                    <img src="${response.url}" alt="qrcodeimage" >;
-            </div>
-            <div class="buttons">
-                <button class="download-btn" id="download_button">
-                   Download<ion-icon
-                     name="arrow-down-circle-outline"
-                     class="download-icon"
-                   ></ion-icon>
-                </button>
-                <button class="share-btn share" id="share">
-                   Share<ion-icon
-                     name="share-social-outline"
-                     class="share-icon"
-                   ></ion-icon>
-                </button>
-            </div>
-      </div>
-            `;
-    spinner.classList.add("hide");
-    spinner.insertAdjacentHTML("afterend", html);
+    spinner.insertAdjacentHTML("afterend", generateHtml(response.url));
     addListeners(blob);
   }
+  spinner.classList.add("hide");
 };
 function addListeners(blob) {
   let download_button = document.querySelector("#download_button");
@@ -84,4 +54,27 @@ function addListeners(blob) {
       }
     });
   });
+}
+function generateHtml(url) {
+  return `
+  <div class="main_div">
+       <div class="image-container" id="img_container">
+                <img src="${url}" alt="qrcodeimage" id="generatedImage">;
+        </div>
+        <div class="buttons">
+            <button class="download-btn" id="download_button">
+               Download<ion-icon
+                 name="arrow-down-circle-outline"
+                 class="download-icon"
+               ></ion-icon>
+            </button>
+            <button class="share-btn share" id="share">
+               Share<ion-icon
+                 name="share-social-outline"
+                 class="share-icon"
+               ></ion-icon>
+            </button>
+        </div>
+  </div>
+        `;
 }
